ab1b46cd43d5bf23459738311aed502af954eec1
Lucas Sanches <sancheseducacional@gmail.com>
2024-03-11 21:56:36 -0300

    novo exercicio sobre if/else

--- 11-03/Condiconal-impar-par.cs
+++ 11-03/Condiconal-impar-par.cs
@@ -8,10 +8,13 @@
             Console.WriteLine("Forneça um número inteiro: ");
             num = Convert.ToInt32(Console.ReadLine());
 
-            if(num % 2 == 0){
-                Console.WriteLine("O núemro é par");
-            }else{
-                Console.WriteLine("O número é impar");
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("O número " + num + " é par ");
+            }
+            else
+            {
+                Console.WriteLine("O número " + num + " é ímpar");
             }
         }
     }
