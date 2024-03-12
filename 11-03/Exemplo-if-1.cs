38d6296ff58bbd38be2b3ebcbf7f0742233a5653
Lucas Sanches <sancheseducacional@gmail.com>
2024-03-11 21:32:16 -0300

    adição de um exemplo

---
+++ 11-03/Exemplo-if-1.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Forneça um número inteiro: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("O número " + num + " é negativo.");
+            }
+            else if(num > 0){
+                Console.WriteLine("O número " + num + " é positivo.");
+            }
+            else{
+                Console.WriteLine("Esse número é zero");
+            }
+        }
+    }
+}
