5003df18b3573d718bf29c7322145f4e61fd55ff
Lucas Sanches <sancheseducacional@gmail.com>
2024-03-25 21:36:50 -0300

    adição de mais três arquivos

---
+++ AnoBissexto.cs
@@ -0,0 +1,14 @@
+﻿int ano;
+
+Console.WriteLine("Insira um ano: ");
+ano = Convert.ToInt32(Console.ReadLine());
+
+if(ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0)){
+    Console.WriteLine("O ano é bissexto");
+}
+else
+{
+    Console.WriteLine("Não é bissexto");
+}
+
+
---
+++ Switch-Case-Meses.cs
@@ -0,0 +1,29 @@
+﻿string mes;
+Console.WriteLine("Digite um mês do ano (Janeiro; Fevereiro, Março, etc): ");
+mes = Console.ReadLine();
+
+switch (mes)
+{
+    case "Janeiro":
+    case "Março":
+    case "Maio":
+    case "Julho":
+    case "Agosto":
+    case "Outubro":
+    case "Dezembro":
+        Console.WriteLine("Este mês tem 31 dias.");
+        break;
+    case "Abril":
+    case "Junho":
+    case "Setembro":
+    case "Novembro":
+        Console.WriteLine("Este mês tem 30 dias.");
+        break;
+    case "Fevereiro":
+        Console.WriteLine("Este mês tem 28 dias ou 29, se o ano for bissexto.");
+        break;
+    default:
+        Console.WriteLine("Nome de mês inválido.");
+        break;
+}
+         
